589aa77fa0fa2b5b7339e323fb94da416e3c9c13
samuel <samuellino2006@gmail.com>
2024-09-26 16:21:51 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown();
+    });
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const endereçoImagem = $("#url-img").val();
+        const novoItem = $('<li style="display:none;"></li>');
+        $(`<img src="${endereçoImagem}"/>`).appendTo(novoItem);
+        $(`<div class="imagem-link">
+            <a href="${endereçoImagem}" target="_blank" title="Ver imagem em tamanho real">Ver foto em tamanho real</a>
+            </div>`).appendTo(novoItem);
+        
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#url-img').val('');
+    })
+
+    $('#cancelar').click(function () {  
+        $('form').slideup();
+    })
+})
